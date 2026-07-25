c5ef9375538cf8d41d88b018304c11a0b440f626
goitlite <goitlite@gmail.com>
2026-07-25 14:36:56 +0700

    a34

--- src/components/features/Carousel.js
+++ src/components/features/Carousel.js
@@ -3,12 +3,12 @@
 import Image from "next/image";
 
 export default function Carousel() {
-  const images = ["/images/Banner25.jpg"];
+  const images = ["/images/banner7.jpg"];
 
   return (
-    // Coba gunakan mt-16 (sekitar 64px). Jika masih terlalu bawah, ganti jadi mt-12 (48px)
-    <div className="max-w-5xl mx-auto mt-7 px-4">
-      <div className="relative w-full h-[250px] md:h-[400px] rounded-2xl overflow-hidden shadow-lg z-0">
+    <div className="max-w-5xl mx-auto mt-[70px] px-4">
+      {/* Mengganti h-[250px] menjadi aspect-[16/9] atau aspect-[2/1] */}
+      <div className="relative w-full aspect-[16/9] md:aspect-[21/9] rounded-2xl overflow-hidden shadow-lg z-0">
         <Image
           src={images[0]}
           alt="Banner"
